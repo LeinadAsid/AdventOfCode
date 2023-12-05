0757492bd92cd4589f39c3a9a7d539d0ae8b151e
Daniel Dias <danynovaisdias@outlook.com>
2023-12-05 01:35:32 -0300

    day 4 part 2 solution

--- 2023/day4/part2.ts
+++ 2023/day4/part2.ts
@@ -34,7 +34,33 @@ function solution(lines: string[]) {
             .filter((chars) => chars !== '')
     );
 
-    for (let cardNumbers of myNumbers) {
-        
+    const copies: Record<number, number> = {};
+
+    for (let [card_index, cardNumbers] of myNumbers.entries()) {
+        let numofwinners = 0;
+
+        for (let [number_index, number] of cardNumbers.entries()) {
+            const isWinner = winningNumbers[card_index].includes(number);
+
+            if (isWinner) {
+                numofwinners += 1;
+            }
+        }
+
+        copies[card_index] ? '' : (copies[card_index] = 0);
+
+        for (let x = 0; x <= copies[card_index]; x++) {
+            for (let i = 1; i <= numofwinners; i++) {
+                copies[card_index + i] = copies[card_index + i] ? copies[card_index + i] + 1 : 1;
+            }
+        }
     }
+
+    const result = Object.entries(copies).map(([key, value]) => {
+        return value + 1;
+    });
+
+    const sum = result.reduce((a, b) => a + b);
+
+    console.log(sum);
 }
